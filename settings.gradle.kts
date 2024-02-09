20d7b3f9c26fc70a5d3b47d872d6c41ae06c4a9a
jankejc <jkornacki@o2.pl>
2024-02-09 14:43:38 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LSO app ver. 3"
+include(":app")
+ 
