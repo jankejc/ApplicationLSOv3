76786650f3244e7a4e43e262b7ad220598742f86
jankejc <jkornacki@o2.pl>
2024-02-22 16:20:20 +0100

    Some layout and start/end picker.

--- app/src/main/java/kornacki/jan/lsoappver3/view/LeaderboardFragment.kt
+++ app/src/main/java/kornacki/jan/lsoappver3/view/LeaderboardFragment.kt
@@ -1,5 +1,6 @@
 package kornacki.jan.lsoappver3.view
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +9,9 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import kornacki.jan.lsoappver3.databinding.FragmentLeaderboardBinding
 import kornacki.jan.lsoappver3.viewModel.LeaderboardViewModel
+import java.time.LocalDate
 
 class LeaderboardFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = LeaderboardFragment()
-    }
     private var _binding: FragmentLeaderboardBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,13 +22,31 @@ class LeaderboardFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
 
         viewModel = ViewModelProvider(this)[LeaderboardViewModel::class.java]
 
+        bindFunctionality()
+
         return binding.root
+    }
+
+    private fun bindFunctionality() {
+        binding.btnChooseStartDay.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, dayOfMonth ->
+                viewModel.prepareStartDateToCompare(year, month, dayOfMonth)
+            }, LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+            datePickerDialog.show()
+        }
+
+        binding.btnChooseEndDay.setOnClickListener {
+            val datePickerDialog = DatePickerDialog(requireContext(), { _, year, month, dayOfMonth ->
+                viewModel.prepareEndDateToCompare(year, month, dayOfMonth)
+            }, LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+            datePickerDialog.show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/kornacki/jan/lsoappver3/viewModel/LeaderboardViewModel.kt
+++ app/src/main/java/kornacki/jan/lsoappver3/viewModel/LeaderboardViewModel.kt
@@ -1,7 +1,17 @@
 package kornacki.jan.lsoappver3.viewModel
 
 import androidx.lifecycle.ViewModel
+import java.time.LocalDateTime
 
-class LeaderboardViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+object LeaderboardViewModel : ViewModel() {
+    private var startDate: LocalDateTime = LocalDateTime.MIN
+    private var endDate: LocalDateTime = LocalDateTime.MIN
+
+    fun prepareStartDateToCompare(year: Int, month: Int, day: Int) {
+        startDate = LocalDateTime.of(year, month, day, 0, 0)
+    }
+
+    fun prepareEndDateToCompare(year: Int, month: Int, day: Int) {
+        endDate = LocalDateTime.of(year, month, day, 0, 0)
+    }
 }
